f158a7b463e45ff82af9cb8d16ccad092e9194e9
oy-paddy <1603645434@qq.com>
2023-07-03 18:29:22 +0800

    增加vw与rem适配方案,可以进行切换

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,7 @@ import { publicData } from "@/composables/useData";
     <h1>首页</h1>
     <h2 class="base-text">scss</h2>
     <div class="flex flex-col relative p-2 b-1">
-      <span class="absolute right-2 top-2 text-base text-red font-bold">px转换为rem会有像素差</span>
+      <span class="absolute right-2 top-2 text-base text-red font-bold">px转换为rem会有像素差，<br/>但是px转换为vw没有像素差</span>
       <div class="flex items-center justify-center w-100 h-25 border 
     border-solid border-cool-gray-400">
         使用unocss后自动转换为rem
--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,14 @@ import UnoCSS from 'unocss/vite'
 
 // 自动管理浏览器前缀
 import autoprefixer from 'autoprefixer'
-// css自动转换为rem，切记需要再main.js中 import 'amfe-flexible'
+// rem方案（转换后会有0.1px的像素差）：css自动转换为rem，切记需要再main.js中 import 'amfe-flexible' 自动设置基准值
 import postCssPxToRem from 'postcss-pxtorem'
 
+// vw方案（无像素差）：px自动转换vw
+import pxtovw from "postcss-px-to-viewport"
+
+// 是否使用vw方案，false:使用rem方案；true:使用vw方案
+const useVwModel = false
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -36,15 +41,29 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        postCssPxToRem({
+        autoprefixer({
+          overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+        }),
+        useVwModel ? pxtovw({
+          unitToConvert: 'px', // 要转化的单位
+          viewportWidth: 750, // UI设计稿的宽度,vant是375。可参考这个：https://juejin.cn/post/6961737808339795975
+          unitPrecision: 6, // 转换后的精度，即小数点位数
+          propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+          viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+          fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+          selectorBlackList: ['ignore-'], // 指定不转换为视窗单位的类名，
+          minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+          mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+          replace: true, // 是否转换后直接更换属性值
+          // exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+          landscape: false, // 是否处理横屏情况
+          exclude: [/node_modules\/vant/i]
+        }) : postCssPxToRem({
           // 自适应，px>rem转换
           rootValue: 75, // 这里代表的是1rem等于多少rootValue的px。75表示750设计稿，37.5表示375设计稿
           propList: ['*'], // 需要转换的属性，这里选择全部都进行转换
           selectorBlackList: ['norem', 'ignore'], // 过滤掉norem-开头的class，不进行rem转换
           mediaQuery: false,  // 允许在媒体查询中转换 px
-        }),
-        autoprefixer({
-          overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
         }),
       ],
     },
