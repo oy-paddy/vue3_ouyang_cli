71923b5cdaa44c1d37138e6d38db6db95198200d
oy-paddy <1603645434@qq.com>
2023-07-04 20:24:31 +0800

    引入vant框架,增加基础模板

---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="">
+    登录模块
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,8 +1,15 @@
 <script lang="ts" setup>
+import { useRoute } from 'vue-router';
+const router = useRouter()
+function toLogin(){
+  router.push('/login');
+}
 </script>
 
 <template>
-  <div class=""></div>
+  <div class="">
+    <button @click="toLogin">去登录</button>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
