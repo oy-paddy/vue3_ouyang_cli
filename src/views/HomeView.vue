fdc8bf229ef3529a60937361ee86f02677c08a8b
oy-paddy <1603645434@qq.com>
2023-07-13 22:53:24 +0800

    提交更新

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,6 @@
 
 <template>
   <div>
-    <BiliBili></BiliBili>
   </div>
 </template>
 
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -2,12 +2,7 @@
 </script>
 <template>
   <div class="about flex-col flex h-full">
-    <ResponsiveBox></ResponsiveBox>
-    <div class="flex flex-row items-center">
-      <input class="w-1/2 h-20 text-2xl rounded ml-2 p-x-5 mr-2" type="text">
-      <!-- <van-button type="primary" size="small">搜索</van-button> -->
-      <button class="rounded p-2 p-x-10 h-20 text-3xl bg-blue-500 hover:bg-blue-600 text-white">搜索</button>
-    </div>
+    
   </div>
 </template>
 
